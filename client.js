fee24514c8f9cd219af8717e3fb521a380ac41d0
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-19 18:29:09 -0400

    snakeInitials

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+const { CONNREFUSED } = require("dns");
 const net = require("net");
 
 const connect = function () {
@@ -9,7 +10,10 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
-    console.log("Connected to server")
+    console.log("Successfully connected to game server")
+  });
+  conn.on("connect", () => {
+    conn.write("Name: LJK");
   });
   conn.on("data", () => {
     console.log("you ded cuz you idled")
