9f0fdce87d9980c258451980ca58de5a70be68d2
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-19 13:10:38 -0400

    movedtoClient.js

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
+const connect = require("./client.js");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
-  }); 
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Connected to server")
-  });
-  conn.on("data", () => {
-    console.log("you ded cuz you idled")
-  })
-  return conn;
-};
-
 console.log("Connecting ...");
-connect();
