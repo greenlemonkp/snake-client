d7066d4cda7a1ee879f20a00788a272e87e3a44a
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-19 13:11:04 -0400

    clientfunctions

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",// IP address here,
+    port: 50541// PORT number here,
+  }); 
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Connected to server")
+  });
+  conn.on("data", () => {
+    console.log("you ded cuz you idled")
+  })
+  return conn;
+};
+connect()
+module.exports = {connect};
