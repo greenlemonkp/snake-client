59d84117b834dbb33da03ab39c3282a3be8967ea
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-20 17:21:41 -0400

    chat

--- input.js
+++ input.js
@@ -27,6 +27,9 @@ const handleUserInput = function (key) {
     if (key === 'd') {
       connection.write("Move: right" )
     }
+    if (key === 'k') {
+      connection.write("Say: Catch me if you can")
+    }
   };
 
 
