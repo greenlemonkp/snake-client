50e7217b159ca7982e4f863cf472305ec849cdae
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-20 13:16:22 -0400

    update

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const connect = require("./client.js");
+const { setupInput } = require("./input");
+console.log("Connecting ...");
 
+connect();
 
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-};
-
-const handleUserInput = function () {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
- setupInput();
+setupInput();
