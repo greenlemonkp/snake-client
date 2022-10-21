eab92a9b6c8a58efabb7a692032c98da22e55153
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-21 14:38:04 -0400

    minor bugs have been fixed and ready

--- play.js
+++ play.js
@@ -1,6 +1,6 @@
 const {connect} = require("./client.js");
 const { setupInput } = require("./input");
-console.log("Connecting ..."); //shows connection in progress
+console.log("Connecting ..."); //shows connection in progress.
 
 
 
